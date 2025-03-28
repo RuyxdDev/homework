e4c5fd9e27ace35f30cf867b32f179cb13badd76
Ruyxd <nicetitsgirl@mail.ru>
2025-03-28 17:22:39 +0300

    Add files via upload

--- e-shop/scripts/main.js
+++ e-shop/scripts/main.js
@@ -18,3 +18,33 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+// Бургер меню
+const burgerIcon = document.querySelector('.burger-icon');
+const nav = document.querySelector('.nav');
+const body = document.querySelector('.body');
+const menuItems = document.querySelectorAll('.header__menu-link'); // Получаем все пункты меню
+
+burgerIcon.addEventListener('click', () => {
+  nav.classList.toggle('nav--active');
+  burgerIcon.classList.toggle('burger-icon--active');
+  body.classList.toggle('body--opened-menu');
+});
+
+// Закрываем меню при клике на пункт меню
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    nav.classList.remove('nav--active');
+    burgerIcon.classList.remove('burger-icon--active');
+    body.classList.remove('body--opened-menu');
+  });
+});
+
+// Закрываем меню при нажатии клавиши Esc
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    nav.classList.remove('nav--active');
+    burgerIcon.classList.remove('burger-icon--active');
+    body.classList.remove('body--opened-menu');
+  }
+});
+
