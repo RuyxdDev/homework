862de5644a36016b39a03131f62b3df132a75e1b
Ruyxd <nicetitsgirl@mail.ru>
2026-01-21 18:58:02 +0300

    Update main-39.js

--- js-5/main-39.js
+++ js-5/main-39.js
@@ -1,61 +1,61 @@
 // Задача 1.
 // Создайте объект person с несколькими свойствами, содержащими информацию о вас. Затем выведите значения этих свойств в консоль.
 
-// const person = {
-//     name: "Daniil",
-//     secondName: "Rutskiy",
-//     age: 2,
-//     eyesColor: "blue"
-// }
+const person = {
+    name: "Daniil",
+    secondName: "Rutskiy",
+    age: 2,
+    eyesColor: "blue"
+}
 
-// const getPar = (obj) =>{
-//     for (const key in obj) {
-//         console.log(obj[key])
-//     }
-//     return 'Конец объекта'
-// }
+const getPar = (obj) =>{
+    for (const key in obj) {
+        console.log(obj[key])
+    }
+    return 'Конец объекта'
+}
 
-// console.log(getPar(person))
+console.log(getPar(person))
 
 // Задача 2.
 // Создайте функцию isEmpty, которая проверяет является ли переданный объект пустым. Если объект пуст - верните true, в противном случае false.
 
-// const person2 = {
-//     name: "Daniil",
-//     secondName: "Rutskiy",
-//     age: 2,
-//     eyesColor: "blue"
-// }
+const person2 = {
+    name: "Daniil",
+    secondName: "Rutskiy",
+    age: 2,
+    eyesColor: "blue"
+}
 
-// const isEmpty = (obj) => {
-//     for (const key in obj) {
-//         return false
-//     }
-//     return true
-// }
+const isEmpty = (obj) => {
+    for (const key in obj) {
+        return false
+    }
+    return true
+}
 
-// console.log(isEmpty(person2))
+console.log(isEmpty(person2))
 
 // Задача 3.
 // Создайте объект task с несколькими свойствами: title, description, isCompleted.
 // Напишите функцию cloneAndModify(object, modifications), которая с помощью оператора spread создает копию объекта и применяет изменения из объекта modifications.
 // Затем с помощью цикла for in выведите все свойства полученного объекта.
 
-// const task = {
-//     title: "Название",
-//     description: "Описание",
-//     isCompleted: "Да"
-// }
-// const task2 = task
-// const cloneAndModify = (object, modifications) => {
-//     modifications = { ...object };
-//     modifications.title = '2';
-//     for (const key in modifications) { 
-//         return modifications
-//     }
-// }
-// console.log(task)
-// console.log(cloneAndModify(task, task2))
+const task = {
+    title: "Название",
+    description: "Описание",
+    isCompleted: "Да"
+}
+const task2 = task
+const cloneAndModify = (object, modifications) => {
+    modifications = { ...object };
+    modifications.title = '2';
+    for (const key in modifications) { 
+        return modifications
+    }
+}
+console.log(task)
+console.log(cloneAndModify(task, task2))
 
 
 // Задача 4.
